eca7679d2a2acdcdff7ebf8c449f4e082896676e
Kanishka-Trivedi <kally651997@gmail.com>
2025-07-25 22:04:55 +0530

    Notifications Page Created

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -43,7 +43,13 @@ const App = () => {
           isOnboarded ? "/" : "/onboarding"
         } />} />
 
-        <Route path="/notifications" element={isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path="/notifications" element={isAuthenticated && isOnboarded ? (
+          <Layout showSidebar={true}>
+            <NotificationsPage />
+          </Layout>
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        ) } />
 
         <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
 
