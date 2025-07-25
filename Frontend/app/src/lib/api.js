35d5718698b994bf79cdd88290ca3db380b6e4e6
Kanishka-Trivedi <kally651997@gmail.com>
2025-07-25 20:00:31 +0530

    Home page created with recommended users

--- Frontend/app/src/lib/api.js
+++ Frontend/app/src/lib/api.js
@@ -21,12 +21,31 @@ try {
   return res.data;
 } catch (error) {
   console.log("Error fetching auth user:", error);
-  
   return null;  
 }
 }
 
 export const completeOnboarding = async (userData) => {
   const response = await axiosInstance.post('/auth/onboarding', userData);
+  return response.data;
+}
+
+export async function getUserFriends() {
+  const response = await axiosInstance.get("/users/friends");
+  return response.data;
+}
+
+export async function getRecommendedUsers() {
+  const response = await axiosInstance.get("/users");
+  return response.data;
+}
+
+export async function getOutgoingFriendReqs() {
+  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+
+export async function sendFriendRequest(userId) {
+  const response = await axiosInstance.post(`/users/friend-request/${userId}`);
   return response.data;
 }
