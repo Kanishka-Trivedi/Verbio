4cfc97d0ff7be15926bdc1d5d654cb9aea72f989
Kanishka-Trivedi <kally651997@gmail.com>
2025-07-26 10:20:49 +0530

    Chat Page Implemented

--- Frontend/app/src/main.jsx
+++ Frontend/app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "stream-chat-react/dist/css/v2/index.css";
 import App from "./App.jsx";
 import "./index.css";
 
