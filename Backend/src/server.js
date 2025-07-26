1bae14d9c334d045580d1f33fc55e8cd45075def
Kanishka-Trivedi <kally651997@gmail.com>
2025-07-26 13:09:39 +0530

    Frontend Link Changed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://verbio-1.onrender.com"],
     credentials: true, 
 })
 );
