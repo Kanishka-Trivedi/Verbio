f7f002aff4f69516dfabe85b81b39a1353a1c114
Kanishka-Trivedi <kally651997@gmail.com>
2026-01-02 15:25:41 +0530

    Link Changed

--- Frontend/app/src/lib/axios.js
+++ Frontend/app/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "https://verbio-vygl.onrender.com/api",
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://verbio-vygl.onrender.com/api",
   withCredentials: true,
 });
