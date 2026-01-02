6347d15a39d02a9f0a1954dcdecec154fabd258a
Kanishka-Trivedi <kally651997@gmail.com>
2026-01-02 16:00:35 +0530

    Issues Solved

--- Frontend/app/src/lib/axios.js
+++ Frontend/app/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "https://verbio-vygl.onrender.com/api",
-  baseURL: "https://verbio-vygl.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
